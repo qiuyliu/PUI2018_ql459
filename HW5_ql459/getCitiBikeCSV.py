992a0f5d6911fe13a2c415516e6afd3457e69db4
qiuyliu <ql459@nyu.edu>
2018-10-08 23:50:31 -0400

    Update getCitiBikeCSV.py

--- HW5_ql459/getCitiBikeCSV.py
+++ HW5_ql459/getCitiBikeCSV.py
@@ -1,20 +1,4 @@
-##Function to download and unpack Citibike data.
-##Moves the data to the directory pointed to by $PUIDATA
-
-import numpy as np
-import os
-import sys
-
-#check if the env variable PUIDATA exists
-if os.getenv("PUIDATA") is None:
-    print ("must set PUIDATA env variable")
-    sys.exit()
-
 def getCitiBikeCSV(datestring):
-    '''Downloads citibike data and unzips it. If the data is downloaded by not unzippeds it zips it. Moves the data to $PUIDATA
-    Arguments:
-        date string as yyyymm
-    '''
     print ("Downloading", datestring)
     ### First I will heck that it is not already there
     if not os.path.isfile(os.getenv("PUIDATA") + "/" + datestring + "-citibike-tripdata.csv"):
